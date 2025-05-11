3dee4d280be4c23beb525756e4bf0f62a9f0ee57
Kxenarinu <ojakivimadli@gmail.com>
2025-05-11 20:04:45 +0300

    added roating, size chaning, colour chaning creature
    you saw the title you know what it be

---
+++ Assets/programming sillieness/feelingcolourful.cs
@@ -0,0 +1,45 @@
+using Unity.FPS.Game;
+using UnityEngine;
+
+namespace AG3566 { 
+public class Mineenemy : MonoBehaviour
+{
+
+    public MeshRenderer Renderer;
+    private float startDelay = 2.0f;
+    private float repeat = 1.0f;
+
+    
+
+    void Start()
+    {
+        transform.position = new Vector3(3, 6, 1);
+        transform.localScale = Vector3.one * 2.5f;
+            InvokeRepeating("changeColor", startDelay, repeat);
+            InvokeRepeating("changeSize", startDelay, repeat/3);
+
+        }
+
+    void Update()
+    {
+            transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    void changeColor()
+    {
+        Material material = Renderer.material;
+
+        material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+
+    }
+    void changeSize()
+        
+        {
+            transform.localScale = Vector3.one * Random.Range(0.1f, 2.5f);
+
+        }
+}
+
+
+
+}
